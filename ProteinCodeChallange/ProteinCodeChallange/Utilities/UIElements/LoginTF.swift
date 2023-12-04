0ddd14a44fc867a5fd2a6bf90cccee253933f308
Namik Karabiyik <89117767+Kiddanube4@users.noreply.github.com>
2023-12-04 17:21:04 +0300

    Custom textfield class add for loginvc

---
+++ ProteinCodeChallange/ProteinCodeChallange/Utilities/UIElements/LoginTF.swift
@@ -0,0 +1,143 @@
+//
+//  LoginTF.swift
+//  ProteinCodeChallange
+//
+//  Created by Namik Karabiyik on 4.12.2023.
+//
+
+import UIKit
+@IBDesignable
+class LoginTF: UITextField {
+    //custom label properties
+    @IBInspectable var errorLabelColor: UIColor = .red {
+        didSet {
+            errorLabel.textColor = errorLabelColor
+            
+        }
+    }
+    
+    @IBInspectable var errorLabelFont: UIFont = .systemFont(ofSize: 8) {
+        didSet {
+            errorLabel.font = errorLabelFont
+            
+        }
+    }
+    
+    
+    @IBInspectable var topLabelTextColor: UIColor = .red {
+        didSet {
+            topLabel.textColor = topLabelTextColor
+            
+        }
+    }
+    
+    @IBInspectable var topLabalFont: UIFont = .systemFont(ofSize: 10) {
+        didSet {
+            topLabel.font = topLabalFont
+            
+        }
+    }
+    
+    
+    @IBInspectable var topLabelText: String = "TopLabel" {
+        didSet {
+            topLabel.text = topLabelText
+            
+        }
+    }
+    
+    @IBInspectable var errorLabeltext: String = "ErrorLabal" {
+        didSet {
+            errorLabel.text = errorLabeltext
+            
+        }
+    }
+    
+    @IBInspectable var isToplabelHiiden: Bool = false {
+        didSet {
+            topLabel.isHidden = isToplabelHiiden
+            
+        }
+    }
+    
+    @IBInspectable var isErrorLabelHidden: Bool = false {
+        didSet {
+            errorLabel.isHidden  = isErrorLabelHidden
+            
+        }
+    }
+    
+    
+    
+    
+    
+    // textfield properties
+    
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+            didSet {
+                layer.cornerRadius = cornerRadius
+                layer.masksToBounds = cornerRadius > 0
+            }
+        }
+    
+    
+    
+    private lazy var topLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = topLabelTextColor
+        label.font =  topLabalFont
+        label.text = topLabelText
+        label.isHidden = isToplabelHiiden
+        return label
+    }()
+    
+    
+    private lazy var errorLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = errorLabelColor
+        label.font = errorLabelFont
+        label.text = errorLabeltext
+        label.isHidden = isErrorLabelHidden
+        return label
+    }()
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTextField()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupTextField()
+    }
+    
+    
+    private func setupTextField() {
+        self.borderStyle = .roundedRect
+        setupTopLabel()
+   
+    }
+    
+    private func setupTopLabel() {
+        
+        addSubview(topLabel)
+                NSLayoutConstraint.activate([
+                    topLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+                    topLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+                    topLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+                    topLabel.heightAnchor.constraint(equalToConstant: 20) // Adjust the height as needed
+                ])
+                
+     
+        
+    }
+       
+
+}
