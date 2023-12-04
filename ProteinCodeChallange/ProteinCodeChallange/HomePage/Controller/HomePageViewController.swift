f571e636b36963946b4af80728565a7e2a009ee9
Namik Karabiyik <89117767+Kiddanube4@users.noreply.github.com>
2023-12-04 19:41:34 +0300

    HomepageViewController add to project

---
+++ ProteinCodeChallange/ProteinCodeChallange/HomePage/Controller/HomePageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageViewController.swift
+//  ProteinCodeChallange
+//
+//  Created by Namik Karabiyik on 4.12.2023.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+}
--- ProteinCodeChallange/ProteinCodeChallange/LoginPage/ViewModel/LoginViewModel.swift
+++ ProteinCodeChallange/ProteinCodeChallange/LoginPage/ViewModel/LoginViewModel.swift
@@ -52,18 +52,20 @@ class LoginViewModel {
     }
     
     
-    func login(userName: String, passWord:String) {
+    func login(userName: String, passWord:String)-> UIViewController? {
         let userCheckQuery = realm?.objects(User.self).filter("userName == %@ AND passWord == %@", userName, passWord)
 
         // Access the first user with the specified username (if it exists)
         if let user = userCheckQuery?.first {
             // Access user properties
             loginDelegate?.login()
+            let homePageController =
         } else {
             loginDelegate?.login(didFailWithError: "Kullanıcı adı veya şifre hatalı")
+            return nil
         }
         
-    
+        return nil
     }
     
     
